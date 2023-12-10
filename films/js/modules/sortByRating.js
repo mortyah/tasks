00186cb6a9b2108e18ea582a873cfdde55e7badf
mortyah <108241020+mortyah@users.noreply.github.com>
2023-12-10 16:49:42 +0300

    Update sortByRating.js

--- films/js/modules/sortByRating.js
+++ films/js/modules/sortByRating.js
@@ -1,7 +1,6 @@
 function sortByRating(data) {
-    const sortedFilms = data.sort((a, b) => a.imdbRating - b.imdbRating)
-    const filmsNames = sortedFilms.map(value => `Title: ${value.title} Rating:${value.imdbRating}`).reverse()
-    return filmsNames.join(", ")
+    const sortedFilms = data.sort((a, b) => a.imdbRating - b.imdbRating).reverse()
+    return sortedFilms
 }
 
 module.exports = {
